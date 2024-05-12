6d18bf1183de50e77d06431db58bcdf32c95d48e
jihopark0727 <101921579+jihopark0727@users.noreply.github.com>
2024-05-12 20:59:09 +0900

    change

--- src/main/java/com/example/demo/DTO/SignUpDto.java
+++ src/main/java/com/example/demo/DTO/SignUpDto.java
@@ -9,13 +9,13 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SignUpDto {
-    private String id;
     private String email;
     private String name;
     private String password;
     private String confirmPassword;
     private String phoneNumber;
     private String userType;
+    private String career;
     private LocalDateTime createdAt;
     private LocalDateTime editedAt;
     private LocalDateTime lastLoginAt;
--- src/main/java/com/example/demo/Entity/UserEntity.java
+++ src/main/java/com/example/demo/Entity/UserEntity.java
@@ -19,24 +19,24 @@ import java.time.LocalDateTime;
 @Table(name="user")				// 본인 테이블명과 맞춰주어야 함
 public class UserEntity {
     @Id
-    private String id;
     private String email;
     private String password;
     private String name;
     private String phoneNumber;
     private String userType;
     private String token;
+    private String career;
     private LocalDateTime createdAt;
     private LocalDateTime editedAt;
     private LocalDateTime lastLoginAt;
 
     public UserEntity(SignUpDto dto) {
-        this.id = dto.getEmail();
         this.email = dto.getEmail();
         this.password = dto.getPassword();
         this.name = dto.getName();
         this.phoneNumber = dto.getPhoneNumber();
         this.userType = dto.getUserType();
+        this.career = dto.getCareer();
         this.token = "";
         this.createdAt = LocalDateTime.now();
         this.editedAt = LocalDateTime.now();
