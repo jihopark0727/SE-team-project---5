b158760cf618f56522bda8820e2b75bcb42394d3
jihopark0727 <101921579+jihopark0727@users.noreply.github.com>
2024-05-17 16:51:04 +0900

    login and, signup complete

--- src/main/java/com/example/demo/Controller/AuthController.java
+++ src/main/java/com/example/demo/Controller/AuthController.java
@@ -11,30 +11,31 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import java.net.URI;
 
-
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-    @Autowired AuthService authService;
+
+    @Autowired
+    AuthService authService;
 
     @PostMapping("/signUp")
-    public ResponseDto<?> signUp(@ModelAttribute SignUpDto requestBody) {
+    public ResponseEntity<?> signUp(@ModelAttribute SignUpDto requestBody) {
         ResponseDto<?> result = authService.signUp(requestBody);
-        return result;
+        HttpHeaders headers = new HttpHeaders();
+        if (result.isResult()) {
+            headers.setLocation(URI.create("/index.html?signup=success")); // 회원가입 후 로그인 화면으로 리다이렉트
+        } else {
+            headers.setLocation(URI.create("/signup.html?error=true")); // 회원가입 실패 시 에러 메시지와 함께 회원가입 화면으로 리다이렉트
+        }
+        return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
     }
 
-//    @PostMapping("/login")
-//    public ResponseDto<?> login(@ModelAttribute LoginDto requestBody) {
-//        ResponseDto<?> result = authService.login(requestBody);
-//        return result;
-//    }
-    // 리다이렉트 로그인 성공하면 홈화면 실패하면 다시 로그인화면
     @PostMapping("/login")
     public ResponseEntity<?> login(@ModelAttribute LoginDto requestBody) {
         ResponseDto<?> result = authService.login(requestBody);
         HttpHeaders headers = new HttpHeaders();
-        if(result.isResult()) {
-            headers.setLocation(URI.create("/home.html"));
+        if (result.isResult()) {
+            headers.setLocation(URI.create("/home.html?login=success"));
         } else {
             headers.setLocation(URI.create("/index.html?error=true")); // 로그인 실패 시 쿼리 파라미터 추가
         }
